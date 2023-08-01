0bd917909e0b10d4fa943eb948b6348674395b53
Aakash Shah <appsparkler@gmail.com>
2023-08-01 10:39:11 +0530

    chore: add new activity for live-barcode-scanning

---
+++ app/src/main/java/com/example/codescanner/LiveBarcodeScanningActivity.kt
@@ -0,0 +1,11 @@
+package com.example.codescanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LiveBarcodeScanningActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_live_barcode)
+    }
+}

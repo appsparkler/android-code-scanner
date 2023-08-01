4fb1af19bf3e0f615ed770642a586489bef06572
Aakash Shah <appsparkler@gmail.com>
2023-08-01 09:27:21 +0530

    chore: wip

---
+++ app/src/main/java/com/example/codescanner/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.codescanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+        setContentView(R.layout.activity_main)
+    }
+}
